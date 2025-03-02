33441ecf3445ea1df3d959aa8ec75c3d09478828
Akinbode Bamigboye <bodejuniorforever@gmail.com>
2025-03-02 14:27:23 +0100

    {+}

--- practice/index.go
+++ practice/index.go
@@ -8,15 +8,21 @@ func main() {
 	var expenses float64
 	fmt.Print("Enter Revenue: ")
 	fmt.Scan(&revenue)
-	fmt.Print("Enter tax rate: ")
-	fmt.Scan(taxRate)
+
 	fmt.Print("Enter Expenses: ")
-	fmt.Scan(expenses)
+	fmt.Scan(&expenses)
+
+	fmt.Print("Enter tax rate: ")
+	fmt.Scan(&taxRate)
+
+
+
+	
 
 	var ebt = revenue - expenses
-	var eat = (revenue -expenses ) -taxRate
-	var ratio = ebt/eat
-	fmt.Print("Ebt",ebt)
-	fmt.Print("Profit",eat)
-	fmt.Print("Ratio",ratio)
+	var profit = ebt * (1-taxRate/100)
+	var ratio = ebt/profit
+	fmt.Println("Ebt",ebt)
+	fmt.Println("Profit",profit)
+	fmt.Println("Ratio",ratio)
 }
